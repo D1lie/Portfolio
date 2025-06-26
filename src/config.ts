3a437980e3a8e92a0dd475b2ab9c3b23b193b00e
Alastor <samael.islies@gmail.com>
2025-06-26 22:43:29 +0530

    config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,6 @@ export const BACKEND_API_URL =
 
 export const FRONTEND_URL =
   import.meta.env.VITE_FRONTEND_URL || "https://iamalastor.netlify.app/";
+
+export const BACKEND_API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:3001";
